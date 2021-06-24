40d25870b1af1caaefacbc54a365ed53f7aeec54
embucketsmeli <emilio.hernandez@mercadolibre.com.mx>
2021-06-24 09:54:12 -0500

    add nueva linea develop

--- src/com/example/demo/DemoApp.java
+++ src/com/example/demo/DemoApp.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class DemoApp {
     public static void main(String[] args) {
         System.out.println("hola a todos");
+        System.out.println("nueva linea");
     }
 }
